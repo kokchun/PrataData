53d4f9bd4c882f48c9e34910f131dcc7ea21d37a
Kokchun <kokchungiang@gmail.com>
2023-01-25 16:23:26 +0100

    makeover 1 finished

--- MYH/utils.py
+++ MYH/utils.py
@@ -20,7 +20,20 @@ class PlotSettings:
     def set_ticklabel_color(self, color=GRAY1):
         self.ax.xaxis.set_tick_params(labelcolor=color)
         self.ax.yaxis.set_tick_params(labelcolor=color)
+        return self
 
+    def set_labels(
+        self,
+        xlabel="",
+        ylabel="",
+        xlabel_position=(0.045, -0.1),
+        ylabel_position=(0.01, 0.93),
+    ):
+        self.ax.set_xlabel(xlabel, color=GRAY2)
+        self.ax.xaxis.set_label_coords(*xlabel_position)
+
+        self.ax.set_ylabel(ylabel, color=GRAY2).set_rotation(0)
+        self.ax.yaxis.set_label_coords(*ylabel_position)
         return self
 
 
